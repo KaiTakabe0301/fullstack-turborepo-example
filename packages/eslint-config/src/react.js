c818deaef92c5e9115076629f7f79065db92ff9e
KaiTakabe0301 <tb12e053.tkb@gmail.com>
2025-05-25 23:00:51 +0900

    feat: Reactの関数コンポーネント定義ルールを追加

--- packages/eslint-config/src/react.js
+++ packages/eslint-config/src/react.js
@@ -38,6 +38,15 @@ export default [
       'react/jsx-uses-vars': 'error',
       'react/prop-types': 'off', // TypeScript handles this
       'react/react-in-jsx-scope': 'off', // Not needed in React 17+
+
+      'react/function-component-definition': [
+        'error',
+        {
+          namedComponents: 'function-declaration',
+          unnamedComponents: 'arrow-function',
+        },
+      ],
+
       'react-hooks/rules-of-hooks': 'error',
       'react-hooks/exhaustive-deps': 'warn',
 
